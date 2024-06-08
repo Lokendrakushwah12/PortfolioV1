c9394030e6e3f29e71774e94df07e794260489fb
Lokendra Kushwah <118094744+Lokendrakushwah12@users.noreply.github.com>
2024-06-08 17:02:46 +0530

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className="h-screen w-full flex justify-center items-center">
+        <h1 className=''>
+          <span className='bg-[hsl(115,85%,85%)] font-[400] text-[hsl(115,99%,35%)] mr-2 rounded-sm px-1'>
+            Frontend developer
+          </span>
+          with a knack <br />
+          for crafting intuitive
+          <span className='instrument-serif-regular-italic px-2'>
+            design tools
+          </span>
+          & <br />
+          seamless
+          <span className='instrument-serif-regular-italic px-2'>
+            Experiences.
+          </span>
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
