361bb1caccb7714e4c0e0e241c196790274ae538
Lokendra Kushwah <118094744+Lokendrakushwah12@users.noreply.github.com>
2024-07-29 01:51:19 +0530

    ok

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,30 +1,18 @@
-import ButtonV1 from "@pixaui/button-v1";
-import ButtonV2 from "@pixaui/button-v2";
-import React from "react";
+import React from 'react'
 
 const Card = ({ title, description, url }) => {
-  return (
-    <>
-      <div className="xm:w-[23rem] xm:h-[15rem] w-[28rem] h-[18rem] md:w-[36rem] p-1 md:h-[24rem] overflow-hidden flex flex-col items-start justify-start bg-[#f5f5f4] shadow-md rounded-xl border shadow-[rgba(0,0,0,0.04)]">
-        <h1 className="px-1 pt-1 md:text-2xl font-[400] leading-5 md:leading-6 text-base">
-          {title}
-        </h1>
-        <h4 className="px-1 md:text-base font-[400] leading-5 md:leading-6 text-sm text-[rgba(36,39,41,0.6)]">
-          {description}
-        </h4>
-        <div className="h-full overflow-hidden w-full">
-          <img
-            className="w-full h-full object-cover bg-[#fafaf9] rounded-lg"
-            src={url}
-          />
-        </div>
-        <div className="flex py-2">
-          <ButtonV1 title="Check out" className="text-xl w-[120px]" />
-          <ButtonV2 title="Check out" className="text-xl w-[120px]" />
-        </div>
-      </div>
-    </>
-  );
-};
+    return (
+        <>
+            <div className='xm:w-[23rem] xm:h-[15rem] w-[28rem] h-[18rem] md:w-[36rem] p-1 md:h-[24rem] overflow-hidden flex flex-col items-start justify-start bg-[#f5f5f4] shadow-md rounded-xl border shadow-[rgba(0,0,0,0.04)]'>
+                <h1 className='px-1 pt-1 md:text-2xl font-[400] leading-5 md:leading-6 text-base'>{title}</h1>
+                <h4 className='px-1 md:text-base font-[400] leading-5 md:leading-6 text-sm text-[rgba(36,39,41,0.6)]'>{description}</h4>
+                <div className="h-full overflow-hidden w-full">
+                    <img className='w-full h-full object-cover bg-[#fafaf9] rounded-lg' src={url} />
+                </div>z
+            </div>
 
-export default Card;
+        </>
+    )
+}
+
+export default Card
